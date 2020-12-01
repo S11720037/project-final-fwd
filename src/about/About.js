f5843fa5dfa0d46e5a318786dba4a31f4bc3b5f3
Arter Tendean <arter.tendean.07@gmail.com>
2020-12-01 16:32:50 +0800

    Update About Page

--- src/about/About.js
+++ src/about/About.js
@@ -4,15 +4,15 @@ class About extends React.Component{
 
 	render(){
 		return(
-			<div className="cointainer-sm mt-4 m-2 p-2 rounded shadow bg-white">
-				<h3 className="text-center">About</h3>
+			<div className="container-sm mt-4 pb-3 rounded shadow bg-white">
+				<br />
+				<h3 className="text-center">Arter Tendean</h3>
 				<hr />
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Saya berasal dari Bumi dan saat ini sedang berkuliah pada salah satu perguruan tinggi swasta yang ada di provinsi saya yaitu Universitas Klabat.</p>
+
+				<p>Website ini saya dibuat sebgai tugas akhir untuk mata kuliah yang saya ambil yaitu Frontend Web Development yang diajarkan oleh dosen saya sir <a href="http://fik.unklab.ac.id/web/Faculty/details/reymon-rotikan" target="_blank">Reymon Rotikan</a> </p>
+
+				<p>Website ini dibuat dengan menggunakan Reactjs, dan semua data yang ditampilkan pada halaman awal diambil dari API <a href="https://www.themoviedb.org/" target="_blank">themoviedb</a>. themoviedb mewajibkan kita untuk menggunakan api key saat akan mengambil data, jadi kita harus mendaftar dan membuat aplikasinya terlebih dahulu sebelum bisa menggunakan layanan API mereka.</p>
 			</div>
 		)
 	}
