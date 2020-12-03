3dd5d5a9fc345f5b8e9e4e10720e77d885cf61ee
Arter Tendean <arter.tendean.07@gmail.com>
2020-12-04 00:39:55 +0800

    Update Error Page

--- src/home/NotFound.js
+++ src/home/NotFound.js
@@ -6,7 +6,8 @@ function NotFound(){
 	return(
 		<div className="container-sm mt-4 main-background rounded shadow p-2 text-center">
 				<h2>Not Found!</h2>
-				<Link to="/">Back to home</Link>
+				<hr />
+				<Link to="/" className="btn btn-success">Back to home</Link>
 		</div>
 	)
 }
